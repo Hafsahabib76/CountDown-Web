4097bd36ab13049c2557e467d9aeb1d111b1eaf4
Hafsa Habib <hafsahabib76@yahoo.cm>
2021-03-01 17:41:55 +0500

    View Time Passed
    3rd Commit

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
-var eventDate = "Mar 4, 2021 00:00:01";
+var eventDate = "Mar 3, 2021 00:00:01";
 var eventTitle = "Siemens";
 var eventSubtitle = "New Organization Day 1";
+var TimePassedLabel = "Time Passed: "
 var showDate = true;
 
 document.getElementById("eventTitle").innerHTML = eventTitle;
@@ -29,18 +30,47 @@ var x = setInterval(
         var seconds = Math.floor((RemainingDateTime % (1000 * 60)) / 1000); 
         
         //Displaying the Result
-        document.getElementById("day").innerHTML =days ; 
+        document.getElementById("day").innerHTML = days ; 
         document.getElementById("hour").innerHTML = ('0' + hours).slice(-2); 
         document.getElementById("minute").innerHTML = ('0' + minutes).slice(-2);
         document.getElementById("second").innerHTML = ('0' + seconds).slice(-2);
 
         //If CountDown is Over. Set the null values.
-        if (RemainingDateTime < 0) { 
-                clearInterval(x); 
-                document.getElementById("day").innerHTML ='0'; 
-                document.getElementById("hour").innerHTML ='00'; 
-                document.getElementById("minute").innerHTML ='00' ;  
-                document.getElementById("second").innerHTML = '00'; } 
+        if (RemainingDateTime < 0) {
+
+            //display Time Passed div when countdown is over
+            var d = document.getElementById("eventTimePassed");
+            document.getElementById("TimePassedLabel").innerHTML = TimePassedLabel;
+            d.style.display = "block"; 
+
+            //setting the values to Zero (0)
+            document.getElementById("day").innerHTML ='0'; 
+            document.getElementById("hour").innerHTML ='00'; 
+            document.getElementById("minute").innerHTML ='00' ;  
+            document.getElementById("second").innerHTML = '00';
+
+            //function to show the time passed values in counter 
+            var checkBox = document.getElementById("checkTimePassed");
+            if (checkBox.checked == true){
+                
+                    //calculating passed time
+                    var PassedDateTime = CurrentDateTime - eventDateTime; 
+        
+                    //Calculation for Days, Hours, Minutes, Seconds
+                    var days = Math.floor(PassedDateTime / (1000 * 60 * 60 * 24)); 
+                    var hours = Math.floor((PassedDateTime %(1000 * 60 * 60 * 24))/(1000 * 60 * 60)); 
+                    var minutes = Math.floor((PassedDateTime % (1000 * 60 * 60)) / (1000 * 60)); 
+                    var seconds = Math.floor((PassedDateTime % (1000 * 60)) / 1000); 
+        
+                    //Displaying the Result
+                    document.getElementById("day").innerHTML = days ; 
+                    document.getElementById("hour").innerHTML = ('0' + hours).slice(-2); 
+                    document.getElementById("minute").innerHTML = ('0' + minutes).slice(-2);
+                    document.getElementById("second").innerHTML = ('0' + seconds).slice(-2);
+            }
+        }
+        
     }, 1000
-); 
+);
+
 
